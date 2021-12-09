5b9d8dbe4de9271a68b05507edd11fdf2fd6e4b7
Hector Ivan Meza Meza <hivan.meza@gmail.com>
2021-12-08 20:57:14 -0600

    subir proyecto a github

---
+++ NorthWind/NorthWind.Entities/Interfaces/Validation/IRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.Interfaces.Validation
+{
+    public interface IRule<T>
+    {
+        IRule<T> AddRequirement(
+            Expression<Func<T,bool>> requirement
+            ,string message);
+
+        IRule<T> AddCollectionItemsValidator(
+            Expression<Func<T, IEnumerable<IFailure>>> itemsValidator);
+    }
+}
---
+++ NorthWind/NorthWind.Entities/Interfaces/Validation/IValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.Interfaces.Validation
+{
+    public interface IValidator<T>
+    {
+        IRule<T> AddRuleFor<TProperty>(
+            Expression<Func<T,TProperty>> expression,
+            bool StopOnFirtFailure);
+
+        bool Validate(T instance);
+
+        List<IFailure> Failures { get; }
+        List<IFailure> SetValidatorFor<ItemsType>(IEnumerable<ItemsType> items,
+            IValidator<ItemsType> validator);
+
+    }
+}
